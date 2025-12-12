d61e75721dea6338878bb84bdb4e572affa97125
perdana <perdanarobert9@gmail.com>
2025-12-12 10:05:09 +0700

    Your descriptive commit message

---
+++ users_202512120740.sql
@@ -0,0 +1,3 @@
+INSERT INTO core_api.users (name,email,email_verified_at,password,remember_token,created_at,updated_at) VALUES
+	 ('John Doe','john@example.com',NULL,'$2y$12$F74cElOFqUbYQuQf0FyFp.QjDV9AqfMA6PCkJkG.cN4s3WBQVJnJO',NULL,'2025-12-11 02:12:19','2025-12-11 02:12:19'),
+	 ('perdana','perdana@example.com',NULL,'$2y$12$VXmdL7LzFjFLNjsjS4OCQ.vDpQJzoyl.BY0xAoO5rHlqxSAwy/itO',NULL,'2025-12-11 02:48:48','2025-12-11 02:48:48');
